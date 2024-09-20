4a8552276a38e527a8f6f26e42d63899814f7657
kaustubha-chaturvedi <kaustubhachaturvedi@outlook.com>
2024-09-20 23:44:19 +0530

    feat: query store history

--- db_utils.py
+++ db_utils.py
@@ -4,3 +4,30 @@ from langchain_community.utilities.sql_database import SQLDatabase
 def load_db(db_uri):
     db = SQLDatabase.from_uri(db_uri)
     return db
+
+def load_store():
+    conn = sqlite3.connect("queries.db")
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS query_history (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            question TEXT,
+            query TEXT,
+            result TEXT
+        )
+    ''')
+    conn.commit()
+    return cursor, conn
+
+def store_query(question, query, result):
+    cursor, conn = load_store()
+    cursor.execute("INSERT INTO query_history (question, query, result) VALUES (?, ?, ?)",
+                   (question, query, result))
+    conn.commit()
+
+def get_query_history():
+    cursor, _ = load_store()
+    cursor.execute("SELECT * FROM query_history")
+    history = cursor.fetchall()
+    return history
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Form
-from db_utils import load_db
+from db_utils import load_db, get_query_history, store_query
 from query_chain import chain_create, sql_infer
 
 app = FastAPI()
@@ -10,6 +10,18 @@ def run_query(db_uri: str = Form(...), question: str = Form(...)):
         db = load_db(db_uri)
         chain = chain_create(db)
         answer = sql_infer(db, chain, question)
+        store_query(question, answer["sql_query"], str(answer["result"]))
         return {"answer": answer["text"]}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/history")
+def get_history():
+    histories = get_query_history()
+    
+    response = {
+        "count": len(histories),
+        "history": dict((idx, {"question": q, "query": query, "result": result}) for idx, q, query, result in histories)   
+    }
+
+    return response
